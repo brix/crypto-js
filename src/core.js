ff1f0032ff58aedfcc44eb6aa7b2c78207a98009
evanvosberg <evanvosberg@inext.me>
2014-06-19 04:16:08 +0200

    Implement a secure random as suggested by @KenanSulayman in issue #7

--- src/core.js
+++ src/core.js
@@ -273,8 +273,27 @@ var CryptoJS = CryptoJS || (function (Math, undefined) {
          */
         random: function (nBytes) {
             var words = [];
-            for (var i = 0; i < nBytes; i += 4) {
-                words.push((Math.random() * 0x100000000) | 0);
+
+            var r = (function (m_w) {
+                var m_w = m_w;
+                var m_z = 0x3ade68b1;
+                var mask = 0xffffffff;
+
+                return function () {
+                    m_z = (0x9069 * (m_z & 0xFFFF) + (m_z >> 0x10)) & mask;
+                    m_w = (0x4650 * (m_w & 0xFFFF) + (m_w >> 0x10)) & mask;
+                    var result = ((m_z << 0x10) + m_w) & mask;
+                    result /= 0x100000000;
+                    result += 0.5;
+                    return result * (Math.random() > .5 ? 1 : -1);
+                }
+            });
+
+            for (var i = 0, rcache; i < nBytes; i += 4) {
+                var _r = r((rcache || Math.random()) * 0x100000000);
+
+                rcache = _r() * 0x3ade67b7;
+                words.push((_r() * 0x100000000) | 0);
             }
 
             return new WordArray.init(words, nBytes);
