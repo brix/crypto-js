1adcb50e99a530b1457988ecabf2475f790c872a
Frederic R <zougi.fr@gmail.com>
2020-06-20 15:43:59 +0100

    lint 🚀

--- src/core.js
+++ src/core.js
@@ -72,7 +72,7 @@ var CryptoJS = CryptoJS || (function (Math, undefined) {
 
             return subtype;
         };
-    }())
+    }());
 
     /**
      * CryptoJS namespace.
@@ -283,8 +283,8 @@ var CryptoJS = CryptoJS || (function (Math, undefined) {
                 }
             } else {
                 // Copy one word at a time
-                for (var i = 0; i < thatSigBytes; i += 4) {
-                    thisWords[(thisSigBytes + i) >>> 2] = thatWords[i >>> 2];
+                for (var j = 0; j < thatSigBytes; j += 4) {
+                    thisWords[(thisSigBytes + j) >>> 2] = thatWords[j >>> 2];
                 }
             }
             this.sigBytes += thatSigBytes;
